09ee9ed33e6af992cb751cdf5cce219bd9c87912
Jimi W <jimi.welling@cs.helsinki.fi>
2017-11-16 11:03:50 +0200

    Add grid model

---
+++ models/grid.rb
@@ -0,0 +1,51 @@
+class Grid
+	def initialize(size = {"x" => 3, "y" => 3})
+		@columns = Array.new size["x"] { Array.new size["y"] {"."}}
+	end
+
+	def width
+		@columns.size
+	end
+
+	def height
+		@columns[0].size unless @columns[0].nil?
+	end
+
+	def column_at (x = 0)
+		@columns[0] unless x >= width
+	end
+
+	def row_at (y = 0)
+		return nil if y >= height
+		row = Array.new width
+		i = 0
+		@columns.each do |column|
+			row[i] = column[i][y]
+			i += 1
+		end
+		row
+	end
+
+	def block_at (x, y)
+		return nil if x >= width or y >= height
+		@columns[x][y]
+	end
+
+	def row_to_s (row = 0)
+		return nil if row >= height
+		out = ""
+		@columns.each do |column|
+			out += column[row].to_s
+		end
+		out
+	end
+
+	def to_s
+		out = ""
+		height.times do|i|
+			out += row_to_s i
+			out += "\n"
+		end
+		out
+	end
+end
