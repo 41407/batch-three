793c68f669755adf1d81cc02302509b4b2124e36
Jimi W <jimi.welling@cs.helsinki.fi>
2017-11-16 09:45:35 +0200

    Add block model

---
+++ models/block.rb
@@ -0,0 +1,18 @@
+class Block
+	def initialize(symbol = '.', attributes = [])
+		@symbol = symbol
+		@attributes = attributes
+	end
+
+	def to_s
+		@symbol
+	end
+
+	def forms_matches?
+		@attributes.include? 'forms_matches'
+	end
+
+	def does_not_fall?
+		@attributes.include? 'does_not_fall'
+	end
+end
